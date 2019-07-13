c6e3801d55b7a099868f734302ee8186d0bae2b9
kumar <krgauravlpu@gmail.com>
2019-07-13 20:33:28 +0530

    routing changes

--- udemyLearning/src/app/app-routing.module.ts
+++ udemyLearning/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'recepies', pathMatch: 'full' },
   { path: 'recepies', loadChildren: './recepies/recepies.module#RecepiesPageModule' }
 ];
 
