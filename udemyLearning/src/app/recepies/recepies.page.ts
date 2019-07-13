663fe2648156de408d56669f0f39b8fd36570899
kumar <krgauravlpu@gmail.com>
2019-07-13 20:25:12 +0530

    initial commit2

---
+++ udemyLearning/src/app/recepies/recepies.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepies',
+  templateUrl: './recepies.page.html',
+  styleUrls: ['./recepies.page.scss'],
+})
+export class RecepiesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
