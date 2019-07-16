777123f1827db8c7991680681c862323230ed04f
kumar <krgauravlpu@gmail.com>
2019-07-16 10:06:22 +0530

    Initial

--- udemyLearning/src/app/app-routing.module.ts
+++ udemyLearning/src/app/app-routing.module.ts
@@ -2,20 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'recepies', pathMatch: 'full' },
-  {
-    path: 'recepies',
-    children: [
-      {
-        path: '',
-        loadChildren: './recepies/recepies.module#RecepiesPageModule'
-      },
-      {
-        path: ':recipeId',
-        loadChildren:
-          './recepies/recipes-details/recipes-details.module#RecipesDetailsPageModule'
-      }
-    ]
+  { path: '', redirectTo: 'recepies', pathMatch: 'full' }
   }
 ];
 
